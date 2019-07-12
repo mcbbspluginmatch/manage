d3ea15e77f3b71efd2993c9c00ec8e404254aab4
韩帅 <btql_hs@bcdsoft.net>
2019-07-12 13:39:50 +0800

    版本5.0.0 新增大量内容

--- src/main/java/cn/handy/utils/ReportUtil.java
+++ src/main/java/cn/handy/utils/ReportUtil.java
@@ -5,9 +5,14 @@ import cn.handy.constants.BaseConfigCache;
 import org.bukkit.Server;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * @author hanshuai
@@ -19,6 +24,11 @@ public class ReportUtil {
      * 统计地址
      */
     private final static String URL_STR = "http://106.12.43.142/api/report";
+
+    /**
+     * 获取ip网址
+     */
+    private static final String QUERY_ADDRESS = "http://www.icanhazip.com";
 
     /**
      * 统计使用情况
@@ -33,7 +43,8 @@ public class ReportUtil {
                     try {
                         Server server = Manage.plugin.getServer();
                         StringBuffer stringBuffer = new StringBuffer();
-                        stringBuffer.append("?port=" + server.getPort());
+                        stringBuffer.append("?ip=" + getOuterNetIp());
+                        stringBuffer.append("&port=" + server.getPort());
                         stringBuffer.append("&maxPlayers=" + server.getMaxPlayers());
                         stringBuffer.append("&motd=" + server.getMotd());
                         stringBuffer.append("&onlineMode=" + server.getOnlineMode());
@@ -71,4 +82,37 @@ public class ReportUtil {
         ps.close();
     }
 
+    /**
+     * 获取外网ip
+     */
+    public static String getOuterNetIp() {
+        String result = "";
+        URLConnection connection;
+        BufferedReader in = null;
+        try {
+            URL url = new URL(QUERY_ADDRESS);
+            connection = url.openConnection();
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "KeepAlive");
+            connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            connection.setConnectTimeout(3000);
+            connection.setReadTimeout(3000);
+            connection.connect();
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (MalformedURLException e) {
+        } catch (IOException e) {
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+        return result;
+    }
 }
