266a2bc77b28a2954b1e149509735bf891d7d2d9
yinyangshi <32478104+euOnmyoji@users.noreply.github.com>
2019-08-03 14:11:24 +0800

    yys评语

--- src/main/java/cn/handy/listener/spawn/SpawnListener.java
+++ src/main/java/cn/handy/listener/spawn/SpawnListener.java
@@ -26,6 +26,7 @@ public class SpawnListener implements Listener {
         if (player.hasPermission("handy.spawn.bed")) {
             return;
         }
+        // 查了下相关代码 主线程访问数据库??? 不缓存下么 (以及好像会有各种null问题) —— yinyangshi
         // 获取spawn地址进行复活点设置传送
         val spawn = Beans.getBeans().getSpawnService().findById(BaseUtil.getSpawnPermission(player));
         if (spawn != null) {
